34ca9d5525f1a43d7d66776e5a1758a0a5532f1f
abrosse <anthony.brosse@social.aston-ecole.com>
2024-07-02 21:52:51 +0200

    Test Codacy

--- src/main/java/fr/joylee/dto/UtilisateurDto.java
+++ src/main/java/fr/joylee/dto/UtilisateurDto.java
@@ -2,7 +2,11 @@ package fr.joylee.dto;
 
 import fr.joylee.enums.RoleEnum;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 @EqualsAndHashCode
 public class UtilisateurDto {
     private int id;
@@ -17,81 +21,5 @@ public class UtilisateurDto {
 
     private String pseudo;
 
-    private RoleEnum role;
-
     private String sexe;
-
-    private String verificationCode;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getPrenom() {
-        return prenom;
-    }
-
-    public void setPrenom(String prenom) {
-        this.prenom = prenom;
-    }
-
-    public String getNom() {
-        return nom;
-    }
-
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-
-    public String getPseudo() {
-        return pseudo;
-    }
-
-    public void setPseudo(String pseudo) {
-        this.pseudo = pseudo;
-    }
-
-    public RoleEnum getRole() {
-        return role;
-    }
-
-    public void setRole(RoleEnum role) {
-        this.role = role;
-    }
-
-    public String getSexe() {
-        return sexe;
-    }
-
-    public void setSexe(String sexe) {
-        this.sexe = sexe;
-    }
-
-    public String getVerificationCode() {
-        return verificationCode;
-    }
-
-    public void setVerificationCode(String verificationCode) {
-        this.verificationCode = verificationCode;
-    }
 }
